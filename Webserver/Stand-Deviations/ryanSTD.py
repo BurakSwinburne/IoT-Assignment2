c23ec063acfe929f7853f784e911ab89804e1139
Nicholas Capaldo <54139870+nick267@users.noreply.github.com>
2020-06-09 15:45:14 +1000

    Add files via upload

---
+++ Webserver/Stand-Deviations/ryanSTD.py
@@ -0,0 +1,27 @@
+import numpy as np
+import requests
+from datetime import time
+
+
+    r = requests.get("http://ec2-52-87-21-173.compute-1.amazonaws.com/devices/" + arduino + "/state")
+    print(r.status_code, r.reason)
+
+    arduino = "ryansArduino"
+
+  if r.status_code == 200:
+        json_data = json.loads(r.text)
+        humidity = []
+        temperature = []
+        light = []
+        date_time = []
+
+     
+        for data in json_data:
+            humidity.append(data[2])
+            temperature.append(data[3])
+            light.append(data[4])
+            date_time.append(data[5])
+            
+            print("the standard deviation for Humidity in ryan's office is: ", npass.std(data[2]))
+            print("the standard deviation for Temperature in ryan's office is: ", npass.std(data[3]))
+            print("the standard deviation for Light in ryan's office is: ", npass.std(data[4]))
